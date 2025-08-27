0d03ac75b0453cc2153eebba2f49363478a4f773
Razor Vedic <vedic.v@razorpay.com>
2025-08-27 19:41:24 +0530

    reset

--- build/copy-ut-assets.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-cp /coverage.out /github/workspace/coverage.out
-cp /test-output.log /github/workspace/test-output.log
-cp /utMetrics.csv /github/workspace/utMetrics.csv
--- docs/README.md
+++
@@ -1,6 +0,0 @@
-# docs
-
-docs keep all the documents of all the services in the repo
-
-## payment docs
-payment docs are kept here either directly or as index.
